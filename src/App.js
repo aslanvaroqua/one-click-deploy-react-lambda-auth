f9934c650a44d0bee015c5fd6e38958229cfd917
Aslan Varoqua <aslan.varoqua@gmail.com>
2018-08-17 08:24:40 -0600

    login form

--- src/App.js
+++ src/App.js
@@ -39,18 +39,10 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       <div className="form_login">
-          <!-- Add a menu:
-           Log in / Sign up - when the user is not logged in
-           Username / Log out - when the user is logged in
-          -->
           <div data-netlify-identity-menu></div>
-
-          <!-- Add a simpler button:
-            Simple button that will open the modal.
-          -->
           <div data-netlify-identity-button>Login with Netlify Identity</div>
       </div>
-      </div>
+    </div>
     );
   }
 }
